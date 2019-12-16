24c2b4cb58d0c392183c9936814ffd6385f46648
felichri <felichri@umich.edu>
2019-12-16 15:19:58 -0500

    Send faster pivot if chair is on the white line, so it turns more

--- src/eyes/src/camera_process.cpp
+++ src/eyes/src/camera_process.cpp
@@ -11,14 +11,16 @@ ros::Publisher george; //the variable formerly known as signal
 ros::Publisher debug;
 
 // pivots handled as bwd then pivot in queue 
-const std::string PIVOTR =  "cBstomf060r060"; 
-const std::string PIVOTL =  "cBstomr060f060";
-const std::string TURNR =   "cBstomf080f050";
-const std::string TURNL =   "cBstomf050f080";
-const std::string FWD =     "cBstomf055f055";
-const std::string SLIGHTR = "cBstomf070f050";
-const std::string SLIGHTL = "cBstomf050f070";
-const std::string GO =      "cDstomf055f055";
+const std::string PIVOTR        = "cBstomf060r060"; 
+const std::string PIVOTL        = "cBstomr060f060";
+const std::string FAST_PIVOTR   = "cBstomf100r100"; 
+const std::string FAST_PIVOTL   = "cBstomr100f100";
+const std::string TURNR         = "cBstomf080f050";
+const std::string TURNL         = "cBstomf050f080";
+const std::string FWD           = "cBstomf055f055";
+const std::string SLIGHTR       = "cBstomf070f050";
+const std::string SLIGHTL       = "cBstomf050f070";
+const std::string GO            = "cDstomf055f055";
 
 
 
@@ -47,15 +49,15 @@ std::string GetLineFromCin() {
 
 int main(int argc, char** argv)
 {
-	ros::init(argc, argv, "camera_process");
+    ros::init(argc, argv, "camera_process");
     
-	ros::NodeHandle Iris; //subscriber
-	ros::NodeHandle Cornea; //publisher
-	ros::Subscriber Handle = Iris.subscribe("/cv_camera/image_mono", 10, chatterCallBack); // 10 was 1000
-	//george = Cornea.advertise<std_msgs::String>("cameron", 1000);
-	george = Iris.advertise<std_msgs::String>("cameron", 10); // 10 was 1000
-	//ros::spin();
-	debug = Iris.advertise<std_msgs::String>("cam_debug", 10);
+    ros::NodeHandle Iris; //subscriber
+    ros::NodeHandle Cornea; //publisher
+    ros::Subscriber Handle = Iris.subscribe("/cv_camera/image_mono", 10, chatterCallBack); // 10 was 1000
+    //george = Cornea.advertise<std_msgs::String>("cameron", 1000);
+    george = Iris.advertise<std_msgs::String>("cameron", 10); // 10 was 1000
+    //ros::spin();
+    debug = Iris.advertise<std_msgs::String>("cam_debug", 10);
 
     // Fee copied over from simple_motors trigger 
     ros::Rate delay_rate(5);
@@ -113,115 +115,115 @@ std::cout << "TOP: " << std::to_string(top_percent_threshold) << std::endl;
 
 void chatterCallBack(const sensor_msgs::Image& view)
 {
-	double topCount = 0;
-	double leftCount = 0;
-	double rightCount = 0;
+    double topCount = 0;
+    double leftCount = 0;
+    double rightCount = 0;
     double middleCount = 0;
-	
-	imageWidth = view.width;
-	imageHeight = view.height;
-	
-	for(int i = 0; i < view.data.size(); i++) //grab top
-	{
-	//std::string command = "i: " + std::to_string(i);
-	
-		if(inTop(i))
-		{
-			//command += "T";
-			topCount += (view.data[i] > threshold);	
-		}
-		if(inLeft(i))
-		{
-		//command += "L";
-			leftCount += (view.data[i] > threshold);
-		}
-		if(inRight(i))
-		{
-		//command += "R";
-			rightCount += (view.data[i] > threshold);
-		}
+    
+    imageWidth = view.width;
+    imageHeight = view.height;
+    
+    for(int i = 0; i < view.data.size(); i++) //grab top
+    {
+    //std::string command = "i: " + std::to_string(i);
+    
+        if(inTop(i))
+        {
+            //command += "T";
+            topCount += (view.data[i] > threshold); 
+        }
+        if(inLeft(i))
+        {
+        //command += "L";
+            leftCount += (view.data[i] > threshold);
+        }
+        if(inRight(i))
+        {
+        //command += "R";
+            rightCount += (view.data[i] > threshold);
+        }
         if (inMiddle(i)) {
             middleCount += (view.data[i] > threshold);
         }
-		//command += "\n";
-		//std_msgs::String stuff;
-		//stuff.data = command;
-		//george.publish(stuff); 
-	}
-	double leftAverage = leftCount/ (imageHeight*sideBandWidth);
-	double rightAverage = rightCount / (imageHeight*sideBandWidth);
-	double topAverage = topCount / (imageWidth*topBandWidth);
+        //command += "\n";
+        //std_msgs::String stuff;
+        //stuff.data = command;
+        //george.publish(stuff); 
+    }
+    double leftAverage = leftCount/ (imageHeight*sideBandWidth);
+    double rightAverage = rightCount / (imageHeight*sideBandWidth);
+    double topAverage = topCount / (imageWidth*topBandWidth);
     double middleAverage = middleCount / num_middle_pixels;
 
-	std::string result;
+    std::string result;
 
 
-	/*
-		forward = f
-		backward = b
-		stop = s 
-		pivotr = R
-		pivotl = L
-		fwdl = l
-		fwdr = r
-		bwdl = e
-		bwdr = i
-		
-	*/
+    /*
+        forward = f
+        backward = b
+        stop = s 
+        pivotr = R
+        pivotl = L
+        fwdl = l
+        fwdr = r
+        bwdl = e
+        bwdr = i
+        
+    */
 
-	if(topAverage > top_percent_threshold && leftAverage > side_percent_threshold && rightAverage > side_percent_threshold) // white in top, left, right
-	{
+    if(topAverage > top_percent_threshold && leftAverage > side_percent_threshold && rightAverage > side_percent_threshold) // white in top, left, right
+    {
         if (favorRight) {
             result = PIVOTR;
         } else {
             result = PIVOTL;
         }
-		
-	}
-	else if(topAverage > top_percent_threshold && leftAverage > side_percent_threshold && rightAverage <= side_percent_threshold) // white in top, left
-	{
-		result = PIVOTR;
-		favorRight = true;
-	}
-	else if(topAverage > top_percent_threshold && leftAverage <= side_percent_threshold && rightAverage > side_percent_threshold) // white in top, right
-	{
-		result = PIVOTL;
-		favorRight = false;
-	}
-	else if(topAverage > top_percent_threshold && leftAverage <= side_percent_threshold && rightAverage <= side_percent_threshold) // white in top
-	{
-		if (favorRight) {
-			result = PIVOTR;
-		}
-		else {
-			result = PIVOTL;
-		}
-	}
-	else if(topAverage <= top_percent_threshold && leftAverage > side_percent_threshold && rightAverage > side_percent_threshold) // white in left, right
-	{
-		result = FWD;
-	}
-	else if(topAverage <= top_percent_threshold && leftAverage > side_percent_threshold && rightAverage <= side_percent_threshold) // white in left
-	{
-		result = TURNR;
+        
+    }
+    else if(topAverage > top_percent_threshold && leftAverage > side_percent_threshold && rightAverage <= side_percent_threshold) // white in top, left
+    {
+        result = PIVOTR;
         favorRight = true;
-	}
-	else if(topAverage <= top_percent_threshold && leftAverage <= side_percent_threshold && rightAverage > side_percent_threshold) // white in right
-	{
-		result = TURNL;
+    }
+    else if(topAverage > top_percent_threshold && leftAverage <= side_percent_threshold && rightAverage > side_percent_threshold) // white in top, right
+    {
+        result = PIVOTL;
         favorRight = false;
-	}
-	else // no white
-	{
-		result = GO; //f = fwd but with lower priority
-	}
-	 
-    if (middleAverage > top_percent_threshold) { // crossed over line --> backup and then pivot
+    }
+    else if(topAverage > top_percent_threshold && leftAverage <= side_percent_threshold && rightAverage <= side_percent_threshold) // white in top
+    {
         if (favorRight) {
             result = PIVOTR;
         }
         else {
             result = PIVOTL;
+        }
+    }
+    else if(topAverage <= top_percent_threshold && leftAverage > side_percent_threshold && rightAverage > side_percent_threshold) // white in left, right
+    {
+        result = FWD;
+    }
+    else if(topAverage <= top_percent_threshold && leftAverage > side_percent_threshold && rightAverage <= side_percent_threshold) // white in left
+    {
+        result = TURNR;
+        favorRight = true;
+    }
+    else if(topAverage <= top_percent_threshold && leftAverage <= side_percent_threshold && rightAverage > side_percent_threshold) // white in right
+    {
+        result = TURNL;
+        favorRight = false;
+    }
+    else // no white
+    {
+        result = GO; //f = fwd but with lower priority
+    }
+     
+    if (middleAverage > top_percent_threshold) { // crossed over line --> backup and then pivot
+        if (favorRight) {
+            result = FAST_PIVOTR;
+        }
+        else {
+            result = FAST_PIVOTL;
         }
     }
 
@@ -245,29 +247,29 @@ void chatterCallBack(const sensor_msgs::Image& view)
     //*/
    
 
-	std_msgs::String stuff;
-	stuff.data = result;
-	george.publish(stuff);
+    std_msgs::String stuff;
+    stuff.data = result;
+    george.publish(stuff);
 
-	std_msgs::String other;
-	other.data = d;
-	debug.publish(other);
+    std_msgs::String other;
+    other.data = d;
+    debug.publish(other);
 
 }
 
 bool inTop(int counter)
 {
-	return counter < (imageWidth*topBandWidth);
+    return counter < (imageWidth*topBandWidth);
 }
 
 bool inLeft(int counter)
 {
-	return (counter % imageWidth) < sideBandWidth;
+    return (counter % imageWidth) < sideBandWidth;
 }
 
 bool inRight(int counter)
 {
-	return (counter % imageWidth) > (imageWidth - sideBandWidth);
+    return (counter % imageWidth) > (imageWidth - sideBandWidth);
 }
 
 bool inMiddle(int counter)
