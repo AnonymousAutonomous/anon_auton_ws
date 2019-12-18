ddbf7e2e3fd92134a57d1f218d70bc00e7fa2976
felichri <felichri@umich.edu>
2019-12-18 11:45:36 -0500

    Changed declaration of old data to fix compiler errors

--- src/eyes/src/camera_process.cpp
+++ src/eyes/src/camera_process.cpp
@@ -23,7 +23,7 @@ const std::string SLIGHTR       = "cBstomf070f050";
 const std::string SLIGHTL       = "cBstomf050f070";
 const std::string GO            = "cDstomf055f055";
 
-uint[640 * 480] old_data; // TODO FIXME: switch to imagewidth * imageheight and make those const?
+uint old_data[640 * 480]; // TODO FIXME: switch to imagewidth * imageheight and make those const?
 
 bool data_read = false;
 
