a243cbbcb6d3ab02d8682dd4eef4be9bb9cdcef3
Fee <felichri@umich.edu>
2020-02-21 13:24:13 -0500

    Automatically create desktop icons for specific pis

--- src/bash_scripts/launch/create_desktop_icons.sh
+++ src/bash_scripts/launch/create_desktop_icons.sh
@@ -8,7 +8,12 @@ echo ${anonautonN}
 
 cd ~/Desktop
 
-IP_FILE = $(cat <<-END
+
+# LIST IP ADDRESS
+
+touch ~/Desktop/list_ip_address.desktop
+
+cat > ~/Desktop/list_ip_address.desktop <<- EOM
 [Desktop Entry]
 Version=1.0
 Exec=/home/$anonautonN/anon_auton_ws/src/bash_scripts/debug/ip_address.sh
@@ -20,11 +25,27 @@ Encoding=UTF-8
 Terminal=true
 Type=Application
 Categories=Application;
-END
-)
-
-touch ~/Desktop/list_ip_address.desktop
-
-echo ${IP_FILE} >> ~/Desktop/list_ip_address.desktop
+EOM
 
 chmod +x ~/Desktop/list_ip_address.desktop
+
+
+# SIMPLE MOTORS
+
+touch ~/Desktop/simple_motors.desktop
+
+cat > ~/Desktop/simple_motors.desktop <<- EOM
+[Desktop Entry]
+Version=1.0
+Exec=/home/$anonautonN/anon_auton_ws/src/bash_scripts/run/simple_motors.sh
+Icon=/home/$anonautonN/anon_auton_ws/src/bash_scripts/icons/ip.png
+Name=Simple Motors
+GenericName=Simple Motors
+Comment=Start roscore, simple motors trigger and listener, connect to Arduino
+Encoding=UTF-8
+Terminal=true
+Type=Application
+Categories=Application;
+EOM
+
+chmod +x ~/Desktop/simple_motors.desktop
